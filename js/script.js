93b038b8237bbc962e1dcb7b32c6c98afa210d8c
santhoshKumar <33511981+santhoshkumarvgds@users.noreply.github.com>
2020-04-27 19:27:38 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+document.body.onload = function () {
+    if (localStorage.length > 0) {
+        var userdataimg = JSON.parse(localStorage.getItem("useradditem"));
+        // alert(userdataimg);
+        var userdata = JSON.parse(localStorage.getItem("useraddname"));
+        var userdataname = "";
+        for (a = 0; a < userdata.length; a++) {
+            userdataname += userdata[a];
+        }
+        for (var countername = 0; countername <
+            1; countername++) {
+            for (var counterimg = userdataimg.length - 1; counterimg >= 0; counterimg--) {
+                if (userdataimg[counterimg].includes("image")) {
+                    isimage(userdataname, userdataimg[counterimg]);
+                } else if (userdataimg[counterimg].includes("video")) {
+                    isimage(userdataname, userdataimg[counterimg]);
+                }
+            }
+        }
+    }
+}
 var modal = document.getElementById("myModal");
 var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
@@ -26,23 +47,11 @@ $("#file").change(function () {
     $file = $("#file");
     i = $(this).get(0).files.length;
     j = 0;
-    /*if (file.files.length != 0) {
-        // for (var i = 0; i < $(this).get(0).files.length; i++){
-
-        if (this.files[0].name.match(/\.(jpg|jpeg|png|gif)$/)) {
-            $var = $("<li><div class='line'><img id='pro' src='images/Profile.jpg' alt='profile'><p>Name > " + $name + "<br><span>Just now</span></p><a class='right'>...</a></div><img id='news' alt=' please check your option and file type'><div class='line1'><i class='far fa-thumbs-up'></i> <p> 0K</p><a class='right'> 0 comments</a><hr><div class='feeds'><a><i class='far fa-thumbs-up'></i> Like</a><a><i class='far fa-comment'></i> Comments</a><a><i class='fas fa-share'></i> Share</a> </div></div></li>");
-        } else if (this.files[0].name.match(/\.(mp4|mov|avi|webm)$/)) {
-            $var = $("<li><div class='line'><img id='pro' src='images/Profile.jpg' alt='profile'><p>Name > " + $name + "<br><span>Just now</span></p><a class='right'>...</a></div><video id='news' alt=' please check your option and file type' autoplay loop controls></video><div class='line1'><i class='far fa-thumbs-up'></i> <p> 0K</p><a class='right'> 0 comments</a><hr><div class='feeds'><a><i class='far fa-thumbs-up'></i> Like</a><a><i class='far fa-comment'></i> Comments</a><a><i class='fas fa-share'></i> Share</a> </div></div></li>");
-        }
-
-        $('.section2 .postnews').prepend($var);*/
-    //  alert($(this).get(0).files[i].name);
     for (k = 0; k < i; k++) {
         var reader = new FileReader();
         reader.onload = function (e) {
             submit[j] = e.target.result;
             j++;
-            //  $("#news").attr('src', e.target.result);
         }
         reader.readAsDataURL(this.files[k]);
     }
@@ -54,18 +63,28 @@ function sa() {
     while (i >= 0) {
         var nametostr = submit[i];
         if (nametostr.includes("image")) {
-            $var = $("<li><div class='line'><img id='pro' src='images/Profile.jpg' alt='profile'><p>Name > " + $name + "<br><span>Just now</span></p><a class='right'>...</a></div><img id='news' alt=' please check your option and file type'><div class='line1'><i class='far fa-thumbs-up'></i> <p> 0K</p><a class='right'> 0 comments</a><hr><div class='feeds'><a><i class='far fa-thumbs-up'></i> Like</a><a><i class='far fa-comment'></i> Comments</a><a><i class='fas fa-share'></i> Share</a> </div></div></li>");
-            $('.section2 .postnews').prepend($var);
-            $("#news").first().attr('src', submit[i]);
+            isimage($name, submit[i]);
         } else if (nametostr.includes("video")) {
-            $var = $("<li><div class='line'><img id='pro' src='images/Profile.jpg' alt='profile'><p>Name > " + $name + "<br><span>Just now</span></p><a class='right'>...</a></div><video id='news' alt=' please check your option and file type' controls></video><div class='line1'><i class='far fa-thumbs-up'></i> <p> 0K</p><a class='right'> 0 comments</a><hr><div class='feeds'><a><i class='far fa-thumbs-up'></i> Like</a><a><i class='far fa-comment'></i> Comments</a><a><i class='fas fa-share'></i> Share</a> </div></div></li>");
-            $('.section2 .postnews').prepend($var);
+            isvideo($name, submit[i]);
             $("#news").first().attr('src', submit[i]);
         }
-
-
         i--;
-
     }
     modal.style.display = "none";
+    if (submit.length) {
+        localStorage.setItem("useraddname", JSON.stringify($name));
+        localStorage.setItem("useradditem", JSON.stringify(submit));
+    }
+}
+
+function isimage(isname, issrc) {
+    $var = $("<li><div class='line'><img id='pro' src='images/Profile.jpg' alt='profile'><p>Name > " + isname + "<br><span>Just now</span></p><a class='right'>...</a></div><img id='news' alt=' please check your option and file type'><div class='line1'><i class='far fa-thumbs-up'></i> <p> 0K</p><a class='right'> 0 comments</a><hr><div class='feeds'><a><i class='far fa-thumbs-up'></i> Like</a><a><i class='far fa-comment'></i> Comments</a><a><i class='fas fa-share'></i> Share</a> </div></div></li>");
+    $('.section2 .postnews').prepend($var);
+    $("#news").first().attr('src', issrc);
+}
+
+function isvideo(isname, issrc) {
+    $var = $("<li><div class='line'><img id='pro' src='images/Profile.jpg' alt='profile'><p>Name > " + isname + "<br><span>Just now</span></p><a class='right'>...</a></div><video id='news' alt=' please check your option and file type' controls></video><div class='line1'><i class='far fa-thumbs-up'></i> <p> 0K</p><a class='right'> 0 comments</a><hr><div class='feeds'><a><i class='far fa-thumbs-up'></i> Like</a><a><i class='far fa-comment'></i> Comments</a><a><i class='fas fa-share'></i> Share</a> </div></div></li>");
+    $('.section2 .postnews').prepend($var);
+    $("#news").first().attr('src', issrc);
 }
